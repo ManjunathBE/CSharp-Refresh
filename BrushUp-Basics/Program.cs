52e2f1fd4e9e740e91fb43508e07b71956c5a447
ManjunathBE <31193651+ManjunathBE@users.noreply.github.com>
2020-06-25 10:22:23 +0530

    Basic Setup

---
+++ BrushUp-Basics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BrushUp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
