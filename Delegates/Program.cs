4312d08c8d6330d4c00df8683b4e745a4c14321c
ManjunathBE <31193651+ManjunathBE@users.noreply.github.com>
2020-06-25 14:15:02 +0530

    Added Func and Action inbuilt deligates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -9,27 +9,28 @@ namespace Delegates
         public delegate bool CheckLengthOfString(string message);
         public delegate int GetLengths(string message);
 
+
         static void Main(string[] args)
         {
-            CheckLengthOfString d = LessThanFive;
-            d += MoreThanFive;
-            d += EqualToFive;
+            //Func<string, bool> lessThanFive = LessThanFive;
+            Func<string, bool> lessThanFive = x => x.Length < 5;
 
-            //List<bool> results = new List<bool>();
-            //foreach(var del in d.GetInvocationList())
-            //{
-            //    results.Add((bool)del.DynamicInvoke("Message"));
-            //}
+            Action<string> printer = Print;
+            printer += PrintTwice;
+            printer("Message");
 
-            List<bool> results = GottaCatchEmAll<bool>(d, "Message");
-            Console.WriteLine(string.Join(", ", results));
+            string[] names = { "Manju", "Puchhi", "Chitte", "Dummy", "Gum", "sis", "Kanadagal" };
+            List<string> lessThanFiveChar = NamesFilter(names, item => item.Length > 5);
+            //List<string> moreThanFive = NamesFilter(names, item => item.Length < 5);
+            List<string> equalToFive = NamesFilter(names, item => item.Length == 5);
 
-            GetLengths g = x => x.Length;
-            g += x => x.Length + 1;
-            g += x => x.Length + 2;
-
-            List<int> lengths = GottaCatchEmAll<int>(g, "Message");
-            Console.WriteLine(string.Join(", ", lengths));
+            //List<string> lessThanFiveChar = NamesFilter(names, LessThanFive);
+            List<string> moreThanFive = NamesFilter(names,MoreThanFive);
+            //List<string> equalToFive = NamesFilter(names, EqualToFive);
+            Console.WriteLine(string.Join(", ", lessThanFiveChar));
+            Console.WriteLine(string.Join(", ", moreThanFive));
+            Console.WriteLine(string.Join(", ", equalToFive));
+            Console.ReadLine();
 
             Console.ReadLine();
         }
@@ -56,6 +57,29 @@ namespace Delegates
             return name.Length == 5;
         }
 
-         
+        private static List<string> NamesFilter(string[] names, Func<string,bool> filters)
+        {
+            List<string> result = new List<string>();
+            foreach (var name in names)
+            {
+                if (filters(name))
+                {
+                    result.Add(name);
+                }
+            }
+            return result;
+        }
+
+        public static void PrintTwice(string message)
+        {
+            Console.WriteLine(message + "1");
+            Console.WriteLine(message + "1");
+        }
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+
     }
 }
