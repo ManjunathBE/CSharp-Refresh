84b22e6dd3d7bdd662460b444a843bb62ac7fddf
ManjunathBE <31193651+ManjunathBE@users.noreply.github.com>
2020-06-25 14:37:25 +0530

    Complex Func Action with Anonymous function

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -6,80 +6,36 @@ namespace Delegates
 {
     class Program
     {
-        public delegate bool CheckLengthOfString(string message);
-        public delegate int GetLengths(string message);
-
-
         static void Main(string[] args)
         {
-            //Func<string, bool> lessThanFive = LessThanFive;
-            Func<string, bool> lessThanFive = x => x.Length < 5;
-
-            Action<string> printer = Print;
-            printer += PrintTwice;
-            printer("Message");
-
             string[] names = { "Manju", "Puchhi", "Chitte", "Dummy", "Gum", "sis", "Kanadagal" };
-            List<string> lessThanFiveChar = NamesFilter(names, item => item.Length > 5);
-            //List<string> moreThanFive = NamesFilter(names, item => item.Length < 5);
-            List<string> equalToFive = NamesFilter(names, item => item.Length == 5);
 
-            //List<string> lessThanFiveChar = NamesFilter(names, LessThanFive);
-            List<string> moreThanFive = NamesFilter(names,MoreThanFive);
-            //List<string> equalToFive = NamesFilter(names, EqualToFive);
-            Console.WriteLine(string.Join(", ", lessThanFiveChar));
-            Console.WriteLine(string.Join(", ", moreThanFive));
-            Console.WriteLine(string.Join(", ", equalToFive));
+            Func<string[], Func<string, bool>, List<string>> extractString = (array, filter) =>
+             {
+                 List<string> result = new List<string>();
+                 foreach (var item in array)
+                 {
+                     if (filter(item))
+                     {
+                         result.Add(item);
+                     }
+                 }
+                 return result;
+             };
+
+            Func<string, bool> lessThanFive = x => x.Length < 5;
+            Func<string, bool> moreThanFive = x => x.Length > 2;
+
+            List<string> namesLessThanFiveChars = extractString(names, lessThanFive);
+            List<string> namesMoreThanFiveChars = extractString(names, moreThanFive);
+
+            Console.WriteLine(string.Join(", ", namesLessThanFiveChars));
+            Console.WriteLine(string.Join(", ", namesMoreThanFiveChars));
+
             Console.ReadLine();
-
-            Console.ReadLine();
         }
 
-        public static List<T> GottaCatchEmAll<T>(Delegate del, object parameter = null)
-        {
-            List<T> result = del.GetInvocationList()
-                                .Select(d => (T)d.DynamicInvoke(parameter))
-                                .ToList();
-            return result;
-        }
-        public static bool MoreThanFive(string name)
-        {
-            return name.Length > 5;
-        }
-
-        public static bool LessThanFive(string name)
-        {
-            return name.Length < 5;
-        }
-
-        public static bool EqualToFive(string name)
-        {
-            return name.Length == 5;
-        }
-
-        private static List<string> NamesFilter(string[] names, Func<string,bool> filters)
-        {
-            List<string> result = new List<string>();
-            foreach (var name in names)
-            {
-                if (filters(name))
-                {
-                    result.Add(name);
-                }
-            }
-            return result;
-        }
-
-        public static void PrintTwice(string message)
-        {
-            Console.WriteLine(message + "1");
-            Console.WriteLine(message + "1");
-        }
-        public static void Print(string message)
-        {
-            Console.WriteLine(message);
-
-        }
+        
 
     }
 }
